266105e83c0f41fbdcc87c2c15ac2a99de7f75ad
Angel Alvarez <alvareza@mit.edu>
2017-06-01 11:08:43 -0700

    started using semantic UI for frontend framework, resdesigned website

--- Client/js/index.js
+++ Client/js/index.js
@@ -1,14 +1,4 @@
-//from projx.mit.edu, thanks vahid
 $(document).ready(function() {
-    // jumping to sections
-    var $bodytag = $('html, body');
-    var sections = ['home', 'about','résumé'];
-    sections.forEach(function (section) {
-        $('.goto-'+section).click(function (e) {
-            $bodytag.animate({
-                scrollTop: $('#'+section).offset().top
-            }, 400);
-        });
-    });
-
+  console.log("document ready");
+  $('.menu .item .button').tab();
 });
